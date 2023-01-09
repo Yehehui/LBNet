74ede8eba6fea628bdad3a7e36b6200c66b0e437
Yehehui <39472436+Yehehui@users.noreply.github.com>
2023-01-09 18:19:53 +0800

    Add files via upload

---
+++ net/poller/DefaultPoller.cc
@@ -0,0 +1,15 @@
+#include "../Poller.h"
+#include "PollPoller.h"
+#include "EPollPoller.h"
+
+#include <stdlib.h>
+
+using namespace muduo::net;
+
+Poller* Poller::newDefaultPoller(EventLoop* loop) {
+    if (::getenv("MUDUO_USE_POLL")) {
+        return new PollPoller(loop);
+    } else {
+        return new EPollPoller(loop);
+    }
+}
